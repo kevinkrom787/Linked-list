fa743f9742053bd8fafa9e4564972b575e3f49f8
Kevin Krom <kevinkrom787@gmail.com>
2018-03-24 14:42:38 -0600

    Add updated styling

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+// dissable buttons
+
+var websiteInput = document.querySelector('website-input');
+var websiteUrl = document.querySelector('website-URL');
+var enterButton = document.querySelector('input-button');
+
+4`123
