78d8ecae87d30b6021d19e533ae48920c646f294
Kevin Krom <kevinkrom787@gmail.com>
2018-03-25 14:23:42 -0600

    Add basic js functionality

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 
 // dissable buttons
 
-var websiteInput = document.querySelector('website-input');
-var websiteUrl = document.querySelector('website-URL');
-var enterButton = document.querySelector('input-button');
+var websiteTitle = document.querySelector('.website-input');
+var websiteUrl = document.querySelector('.url-input');
+var enterButton = document.querySelector('.input-button');
+var readButton = document.querySelector('.read');
+var cardTitle = document.querySelector('.card-one-title');
+var cardUrl = document.querySelector('.url-output')
+// var titleInput = document.querySelector('.website-input');
 
-4`123
+  enterButton.addEventListener('click', storeInput);
+
+
+function storeInput() {
+  var titleInput = document.querySelector('.website-input');
+  var websiteInput = document.querySelector('.url-input');
+  cardTitle.innerText = titleInput.value;
+  cardUrl.innerText = websiteInput.value;
+  
+}
+
+
+function addNewCard() {
+  var card = document.createElement('.cards');
+  document.body.appendChild(cards);
+}
