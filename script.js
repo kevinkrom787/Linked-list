4f21b02db8809d66e2c26b4ec2204fd9193db406
Todd Ayres <todd.ayres@ymail.com>
2018-03-28 09:49:49 -0600

    Added delete functionality - TA, Added read functionality KK

--- script.js
+++ script.js
@@ -3,41 +3,71 @@
 var websiteTitle = document.querySelector('.website-input');
 var websiteUrl = document.querySelector('.url-input');
 var enterButton = document.querySelector('.input-button');
-var readButton = document.querySelector('.read');
+var readButton = document.querySelector('.reading');
 var cardTitle = document.querySelector('.card-one-title');
 var cardUrl = document.querySelector('.url-output')
+var thingsToShow = document.querySelector('.container-right');
 // var titleInput = document.querySelector('.website-input');
-
 
 // enterButton.addEventListener('click', mySecondFunction);
 // readButton.addEventListener('click', markRead);
 
 
 function storeInput() {
-  console.log("wattup")
- var titleInput = document.querySelector('.website-input');
- var websiteInput = document.querySelector('.url-input');
+var titleInput = document.querySelector('.website-input');
+var websiteInput = document.querySelector('.url-input');
 
- var title = websiteTitle.value
- var url = websiteUrl.value
- 
- var thingsToShow = document.querySelector('.container-right');
-  var bookmark = document.createElement('div')
-  bookmark.innerHTML = 
-    `<article class="cards" id=${Date.now()}>
-      <h2 class="card-one-title">${title}</h2>
-        <hr>
-      <div class="url-output"><a href="${url}" class="website-url">${url}</a></div>
-        <hr>
-        <span class="read-delete">
-        <button class="read-delete">Read</button>
-        <button class="read-delete">Delete</button>
-      </span>
-     </article>`;
-     thingsToShow.prepend(bookmark)
+var title = websiteTitle.value
+var url = websiteUrl.value
+
+var bookmark = document.createElement('div')
+bookmark.innerHTML =
+  `<article class="cards" id=${Date.now()}>
+    <h2 class="card-one-title">${title}</h2>
+      <hr>
+    <div class="url-output"><a href="${url}" class="website-url">${url}</a></div>
+      <hr>
+     <span class="read-delete">
+      <button class="reading">Read</button>
+      <button class="read-delete">Delete</button>
+    </span>
+   </article>`;
+   thingsToShow.prepend(bookmark)
 
 websiteTitle.value = '';
- websiteUrl.value = '';
+websiteUrl.value = '';
 }
 
-  enterButton.addEventListener('click', storeInput);
+
+$('.container-right').on('click', 'article .reading', function() {
+ $(this).toggleClass('read');
+ $(this).closest('article').toggleClass('background-toggle');
+});
+
+
+function deleteCard() {
+var deleteButton = event.target
+if (event.target.matches(".read-delete")) {
+  deleteButton.closest(".cards").remove()
+} 
+}
+
+enterButton.addEventListener('click', storeInput);
+
+thingsToShow.addEventListener("click", deleteCard);
+
+
+
+
+// create completeCard function
+// if user input is '' || websiteUrl = ''
+// provide alert and remove card
+
+//make shit responsive in CSS.
+
+
+// Button for creating links should be disabled if no contents in url fields
+
+// application should be able to keep count of total number of links on page
+
+//keep total count of read and unread links on page.
